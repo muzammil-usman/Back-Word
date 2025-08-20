7f0f21b14793d633a1df2bc98cfa0cd56496527e
Muzammil Usman <uraza2520@gmail.com>
2025-08-20 18:13:18 +0500

    warrant

--- script.js
+++ script.js
@@ -12,9 +12,6 @@ onAuthStateChanged(auth, (user) => {
     window.location.replace("./Feed/feed.html");
   } else {
     console.log("user andar nahi hey");
-    if (location.pathname !== "index.html") {
-      window.location.replace("index.html");
-    }
   }
 });
 
